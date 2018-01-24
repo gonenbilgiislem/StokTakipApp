b3d9d83230ba75c31da7376d00c6530d6cd1160c
Barış Özen <bilgiislem@balikesirgonen.bel.tr>
2018-01-25 01:42:31 +0300

    Proje dosyası ekle.

---
+++ StokTakipApp/Controllers/DepoController.cs
@@ -0,0 +1,92 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StokTakipApp.Controllers
+{
+    public class DepoController : Controller
+    {
+        // GET: Depo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        StokTakipApp.Models.StokAppDbEntities db = new StokTakipApp.Models.StokAppDbEntities();
+
+        [ValidateInput(false)]
+        public ActionResult GridView1Partial()
+        {
+            var model = db.Depo;
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialAddNew(StokTakipApp.Models.Depo item)
+        {
+            var model = db.Depo;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialUpdate(StokTakipApp.Models.Depo item)
+        {
+            var model = db.Depo;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var modelItem = model.FirstOrDefault(it => it.Id == item.Id);
+                    if (modelItem != null)
+                    {
+                        this.UpdateModel(modelItem);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialDelete(System.Int32 Id)
+        {
+            var model = db.Depo;
+            if (Id >= 0)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.Id == Id);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+    }
+}
---
+++ StokTakipApp/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="StokTakipApp.MvcApplication" Language="C#" %>
